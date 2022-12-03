bfdcf1d660b70aabce5ab50881639a17d288e168
AllanEl <allanrnelias@gmail.com>
2022-12-03 00:51:44 -0500

    added category and product to schema file

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,19 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
+-- Add category table --
+CREATE TABLE category(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  category_name VARCHAR(100) NOT NULL
+);
+
+
+-- Add product table --
+CREATE TABLE product(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  price DECIMAL NOT NULL, -- validate it being decimal here --
+  stock INT NOT NULL DEFAULT 10
+);
