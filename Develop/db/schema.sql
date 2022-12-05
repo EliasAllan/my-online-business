4ae1a825094c9eda37e1ed77667538a1f02c099d
AllanEl <allanrnelias@gmail.com>
2022-12-05 03:30:43 -0500

    finished schema file

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -18,5 +18,26 @@ CREATE TABLE product(
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   product_name VARCHAR(100) NOT NULL,
   price DECIMAL NOT NULL, -- validate it being decimal here --
-  stock INT NOT NULL DEFAULT 10
+  stock INT NOT NULL DEFAULT 10,
+  category_id INT ,
+  FOREIGN KEY (category_id)
+  REFERENCES category(id)
+);
+
+-- Add Tag table --
+CREATE TABLE tag(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  tag_name VARCHAR(100) NOT NULL
+);
+
+-- Add ProductTag table --
+CREATE TABLE productTag(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  product_id INT,
+  FOREIGN KEY (product_id)
+  REFERENCES product(id),
+  tag_id INT,
+  FOREIGN KEY (tag_id)
+  REFERENCES tag(id),
+
 );
